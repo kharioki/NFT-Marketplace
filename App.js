6679c93bf3e71b5b378a102c65af3790f8d622b8
kharioki <tony.kharioki@gmail.com>
2022-11-05 09:27:25 +0300

    details screen

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
--- screens/Details.js
+++ screens/Details.js
@@ -1,10 +1,89 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
 
-const Details = () => {
+import { COLORS, SHADOWS, SIZES, FONTS, assets } from '../constants'
+import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsBid, DetailsDesc } from '../components'
+import { Fragment } from 'react'
+
+const DetailsHeader = ({ data, navigation }) => (
+  <View style={{ width: '100%', height: 373 }} >
+    <Image
+      source={data.image}
+      resizeMode="cover"
+      style={{ width: '100%', height: '100%' }}
+    />
+
+    <CircleButton
+      imgUrl={assets.left}
+      handlePress={() => navigation.goBack()}
+      left={15}
+      top={StatusBar.currentHeight + 10}
+    />
+
+    <CircleButton
+      imgUrl={assets.heart}
+      handlePress={() => navigation.goBack()}
+      right={15}
+      top={StatusBar.currentHeight + 10}
+    />
+  </View>
+)
+
+const Details = ({ route, navigation }) => {
+  const { data } = route.params
+  console.log(data);
+
   return (
-    <View>
-      <Text>Details</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+
+      <View style={{
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+        paddingVertical: SIZES.font,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        zIndex: 1,
+      }}>
+        <RectButton
+          minWidth={170}
+          fontSize={SIZES.large}
+          {...SHADOWS.dark}
+        />
+      </View>
+
+      <FlatList
+        data={data.bids}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
+        ListHeaderComponent={() => (
+          <Fragment>
+            <DetailsHeader data={data} navigation={navigation} />
+            <SubInfo />
+            <View style={{ padding: SIZES.font }}>
+              <DetailsDesc data={data} />
+
+              {data.bids.length > 0 && (
+                <Text style={{
+                  fontSize: SIZES.font,
+                  color: COLORS.primary,
+                  fontFamily: FONTS.semiBold,
+                }}>
+                  Current Bids
+                </Text>
+              )}
+            </View>
+          </Fragment>
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
