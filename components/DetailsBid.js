aed951dd4055f8dc7e62f7a045a70f30c8aab2b1
kharioki <tony.kharioki@gmail.com>
2022-11-05 09:41:03 +0300

    search functionality

--- components/DetailsBid.js
+++ components/DetailsBid.js
@@ -1,7 +1,7 @@
 import { View, Text, Image } from 'react-native'
 
 import { EthPrice } from './SubInfo'
-import { COLORS, SIZES, FONTS, assets } from '../constants'
+import { COLORS, SIZES, FONTS } from '../constants'
 
 const DetailsBid = ({ bid }) => {
   return (
